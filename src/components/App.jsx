3dc56f879c51fb431c8bd4b872bff379739100c6
mahbha <43632182+mahbha@users.noreply.github.com>
2021-10-03 10:49:04 +0530

    Mouse click, over, out event and style with useState

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  const [headingText, setHeadingText] = useState("Hello");
+  const [isMouseOver, setMouseOver] = useState(false);
+
+  function handleClick() {
+    setHeadingText("Submitted");
+  }
+
+  function handleMouseOut() {
+    setMouseOver(false);
+  }
+
+  function handleMouseOver() {
+    setMouseOver(true);
+  }
+
   return (
     <div className="container">
-      <h1>Hello</h1>
+      <h1>{headingText}</h1>
       <input type="text" placeholder="What's your name?" />
-      <button>Submit</button>
+      <button
+        style={{ backgroundColor: isMouseOver ? "black" : "white" }}
+        onClick={handleClick}
+        onMouseOut={handleMouseOut}
+        onMouseOver={handleMouseOver}
+      >
+        Submit
+      </button>
     </div>
   );
 }
